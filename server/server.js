04ead5b965d23c406978c9dc53f1292f64dc58d1
Vijay Ramdass <xpressenterprisellc@gmail.com>
2019-04-17 22:53:56 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
+const {ObjectID} = require('mongodb'); 
 
 var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo'); 
@@ -28,6 +29,35 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id; 
+
+  //Valid id using isValid
+    // 404 if todo not found send back empty send
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send(); 
+  }
+
+  // findByID
+  Todo.findById(id).then((todo) => {
+    //success
+    // if no todo - send back 404 with empy body 
+    if(!todo) {
+      res.status(404); 
+    }
+    
+    // if todo - send it back
+    res.send(todo); 
+  }).catch((e) => {
+    res.status(404).send(); 
+  })
+    
+      
+    //error 
+      //400 - send empty body back
+})
 
 app.listen(3000, () => {
   console.log(`Started on port 3000`);
