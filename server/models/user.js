4c24ff90db85b82ba23d73e00147f951357d91fe
Vijay Ramdass <xpressenterprisellc@gmail.com>
2019-06-12 12:02:59 -0400

    Hashed user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs'); 
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -69,5 +70,22 @@ UserSchema.statics.findByToken = function (token) {
 };
 
 var User = mongoose.model('User', UserSchema);
+
+UserSchema.pre('save', function(next) {
+  var user = this; 
+
+  if (user.isModified('password')) {
+    //user.password 
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash; 
+        next();
+      });
+    });
+  } else {
+    next(); 
+  }
+  
+});
 
 module.exports = {User}
