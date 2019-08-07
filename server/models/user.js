65d29e3ee9432d42a1f47cb9cd199c5f05a4e468
Vijay Ramdass <xpressenterprisellc@gmail.com>
2019-08-07 16:59:40 -0400

    Add tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -90,6 +100,8 @@ UserSchema.statics.findByCredentials = function(email, password) {
     });
   });
 };
+
+
 
 var User = mongoose.model('User', UserSchema);
 
